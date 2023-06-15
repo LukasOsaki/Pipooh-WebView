3f5cf841b09d2b3b7b9b2988edd180de0036ca87
Lukas Osaki <lukas.osaki@gmail.com>
2023-06-15 10:47:24 -0300

    Ajuste link externo e icone

--- App.js
+++ App.js
@@ -5,9 +5,21 @@ import { WebView } from 'react-native-webview';
 export default function App() {
   const [url, setUrl] = useState('');
   const [go, setGo] = useState(false);
-  return(
+  return (
     <WebView
       source={{ uri: "https://app.pipooh.com.br/" }}
+      onShouldStartLoadWithRequest={(event) => {
+        const { url } = event;
+        if (!url.includes("app.pipooh")) {
+          if (url.startsWith('http://') || url.startsWith('https://')) {
+            // URLs externos, abrir em um navegador separado
+            Linking.openURL(url);
+            return false; // impede o carregamento da URL no WebView
+          }
+        } 
+        // URLs internos, permitir o carregamento no WebView
+        return true;
+      }}
       // style={{ marginTop: 20,
       // backgroundColor: #01b1af }}
       style={[{ marginTop: 35 }, { backgroundColor: '#01b1af' }]}
